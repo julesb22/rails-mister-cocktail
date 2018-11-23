803ae00a9247daccbe1a8b53f1e99add4350ea10
Jules Boiteux <julesboiteux@gmail.com>
2018-11-23 17:41:17 +0100

    last commit of the day - maybe

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,14 @@
 require 'json'
 require 'open-uri'
 
-url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-ingredient_serialized = open(url).read
-ingredient = JSON.parse(ingredient_serialized)
+# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# ingredient_serialized = open(url).read
+# ingredient = JSON.parse(ingredient_serialized)
 
-ingredient_array = ingredient["drinks"]
+# ingredient_array = ingredient["drinks"]
 
-  ingredient_array.each do |ingredient|
-    ingredient_1 = Ingredient.create(name: ingredient["strIngredient1"])
-  end
+  # ingredient_array.each do |ingredient|
+  #   ingredient_1 = Ingredient.create(name: ingredient["strIngredient1"])
+  # end
 
-
-
+@cocktails = Cocktail.all
